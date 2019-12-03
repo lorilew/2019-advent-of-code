dfd126b66493ab89547097a339934c3905350546
lorilew <lorilew123@gmail.com>
2019-12-03 21:31:45 +0000

    solution 3 part 2

--- solutions/day_3.py
+++ solutions/day_3.py
@@ -1,37 +1,81 @@
-
 def generate_path(wire):
-    input = wire.split(',')
+    input = wire.split(",")
     path = [(0, 0)]
     for instruction in input:
         direction = instruction[0]
         length = int(instruction[1:])
         current_x, current_y = path[-1]
-        if direction == 'R':
+        if direction == "R":
             [path.append((current_x + i, current_y)) for i in range(1, length + 1)]
-        elif direction == 'L':
+        elif direction == "L":
             [path.append((current_x - i, current_y)) for i in range(1, length + 1)]
-        elif direction == 'U':
+        elif direction == "U":
             [path.append((current_x, current_y + i)) for i in range(1, length + 1)]
-        elif direction == 'D':
+        elif direction == "D":
             [path.append((current_x, current_y - i)) for i in range(1, length + 1)]
         else:
-            raise Exception(f'Unknown input: {direction}')
+            raise Exception(f"Unknown input: {direction}")
+    return path
+
+
+def generate_path_with_wire_length(wire):
+    input = wire.split(",")
+    path = [(0, 0, 0)]
+    for instruction in input:
+        direction = instruction[0]
+        step_count = int(instruction[1:])
+        current_x, current_y, current_length = path[-1]
+        if direction == "R":
+            [
+                path.append((current_x + i, current_y, current_length + i))
+                for i in range(1, step_count + 1)
+            ]
+        elif direction == "L":
+            [
+                path.append((current_x - i, current_y, current_length + i))
+                for i in range(1, step_count + 1)
+            ]
+        elif direction == "U":
+            [
+                path.append((current_x, current_y + i, current_length + i))
+                for i in range(1, step_count + 1)
+            ]
+        elif direction == "D":
+            [
+                path.append((current_x, current_y - i, current_length + i))
+                for i in range(1, step_count + 1)
+            ]
+        else:
+            raise Exception(f"Unknown input: {direction}")
     return path
 
 
 def get_min_intersection(input_1, input_2):
-        path_1 = generate_path(input_1)[1:]
-        path_2 = generate_path(input_2)[1:]
-        common = set(path_1).intersection(set(path_2))
-        distances = [abs(x) + abs(y) for x, y in common]
-        return min(distances)
+    path_1 = generate_path(input_1)[1:]
+    path_2 = generate_path(input_2)[1:]
+    common = set(path_1).intersection(set(path_2))
+    distances = [abs(x) + abs(y) for x, y in common]
+    return min(distances)
+
+
+def get_min_intercept_by_wire_length(input_1, input_2):
+    path_1 = generate_path_with_wire_length(input_1)[1:]
+    path_2 = generate_path_with_wire_length(input_2)[1:]
+    path_1_lookup_length = {(x, y): l for x, y, l in path_1}
+    path_2_lookup_length = {(x, y): l for x, y, l in path_2}
+
+    common = set([(x, y) for x, y, l in path_1]).intersection(
+        set([(x, y) for x, y, l in path_2])
+    )
+    lengths = [
+        path_1_lookup_length[(x, y)] + path_2_lookup_length[(x, y)] for x, y in common
+    ]
+    return min(lengths)
 
 
 def sol_3():
-    with open('../resources/input_3.txt') as f:
+    with open("../resources/input_3.txt") as f:
         input_1 = f.readline()
         input_2 = f.readline()
-        return get_min_intersection(input_1, input_2)
-
-
-print(sol_3())
+        print(get_min_intersection(input_1, input_2))
+        print(get_min_intercept_by_wire_length(input_1, input_2))
--- solutions/tests/test_day_3.py
+++ solutions/tests/test_day_3.py
@@ -1,19 +1,33 @@
 import unittest
 
-from solutions.day_3 import get_min_intersection
+from solutions.day_3 import get_min_intersection, get_min_intercept_by_wire_length
 
 
 class Day3testCase(unittest.TestCase):
     def test_sol_3(self):
-        input_1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'
-        input_2 = 'U62,R66,U55,R34,D71,R55,D58,R83'
+        input_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+        input_2 = "U62,R66,U55,R34,D71,R55,D58,R83"
         expected_output = 159
         output = get_min_intersection(input_1, input_2)
         self.assertEqual(output, expected_output)
 
     def test_sol_3_again(self):
-        input_1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51'
-        input_2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+        input_1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+        input_2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
         expected_output = 135
         output = get_min_intersection(input_1, input_2)
+        self.assertEqual(output, expected_output)
+
+    def test_sol_3_2(self):
+        input_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+        input_2 = "U62,R66,U55,R34,D71,R55,D58,R83"
+        expected_output = 610
+        output = get_min_intercept_by_wire_length(input_1, input_2)
+        self.assertEqual(output, expected_output)
+
+    def test_sol_3_2_again(self):
+        input_1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+        input_2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+        expected_output = 410
+        output = get_min_intercept_by_wire_length(input_1, input_2)
         self.assertEqual(output, expected_output)
